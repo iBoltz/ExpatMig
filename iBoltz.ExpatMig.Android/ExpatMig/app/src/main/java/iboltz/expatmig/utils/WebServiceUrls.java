3335d9d27f4849143987b5cc8e2ef8dc5a539ffa
vellathurachi <vennivelu@gmail.com>
2016-07-28 16:31:51 +0530

    Chat interface added for andorid

--- iBoltz.ExpatMig.Android/ExpatMig/app/src/main/java/iboltz/expatmig/utils/WebServiceUrls.java
+++ iBoltz.ExpatMig.Android/ExpatMig/app/src/main/java/iboltz/expatmig/utils/WebServiceUrls.java
@@ -5,30 +5,30 @@ public class WebServiceUrls {
     public static final String WebSiteUrl = "food.iboltz.com";
 
     //Verify Update - But not used
-    public static final String AppVersionSyncService = "http://192.168.1.109:4002/apps/syncversion/";
+    public static final String AppVersionSyncService = "http://192.168.1.105:4004/apps/syncversion/";
 
 
     //Start Up
     public static final String UserLoginService = "http://192.168.1.118:5001/api/useraccess/login";
-    public static final String UserCreationByDeviceService = "http://192.168.1.109:4002/usersbyDeviceID/getorcreateuser";
-    public static final String GetAccessTokenWithUser = "http://192.168.1.109:4002/users/getaccesstokenwithuser/";
-    public static final String GetRestaurantByUserID = "http://192.168.1.109:4002/restaurants/getRestaurantByUserID/";
+    public static final String UserCreationByDeviceService = "http://192.168.1.105:4004/usersbyDeviceID/getorcreateuser";
+    public static final String GetAccessTokenWithUser = "http://192.168.1.105:4004/users/getaccesstokenwithuser/";
+    public static final String GetRestaurantByUserID = "http://192.168.1.105:4004/restaurants/getRestaurantByUserID/";
     public static final String GcmDeviceRegistration = "http://192.168.1.118:5001/api/userdevices";
 
-    //Voided Orders
-    public static final String GetVoidOrders = "http://192.168.1.109:4002/orders/getvoidorders/";
-
+   //Chat Page
+   public static final String CreateTopics = "http://192.168.1.105:4004/api/Topics";
+    public static final String GetTopicsByThreadID = "http://192.168.1.105:4004/api/Topics/AllTopicsForThisThread/";
 
     //My Profile
-    public static final String UserUpdationService = "http://192.168.1.109:4002/users/updateuser/";
-    public static final String UpdateUserDetails = "http://192.168.1.109:4002/users/UpdateuserDetail/";
+    public static final String UserUpdationService = "http://192.168.1.105:4004/users/updateuser/";
+    public static final String UpdateUserDetails = "http://192.168.1.105:4004/users/UpdateuserDetail/";
 
     //Settings Page
-    public static final String HandShakeGCM = "http://192.168.1.109:4002/orders/handshakegcm/";
-    public static final String GetTestService = "http://192.168.1.109:4002/users/testservice/";
+    public static final String HandShakeGCM = "http://192.168.1.105:4004/orders/handshakegcm/";
+    public static final String GetTestService = "http://192.168.1.105:4004/users/testservice/";
 
     //Log Helper
-    public static final String SendErrorEmail = "http://192.168.1.109:4002/apps/senderrormail/";
+    public static final String SendErrorEmail = "http://192.168.1.105:4004/apps/senderrormail/";
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
